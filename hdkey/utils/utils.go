d031119149244bd3c89ae48275e9bf0172113908
pj <pj.kyosyou@outlook.jp>
2018-10-28 16:46:44 +0900

    hdkey: add comment

--- hdkey/utils/utils.go
+++ hdkey/utils/utils.go
@@ -8,19 +8,22 @@ var (
 	bitcoinBase58Strings = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 )
 
+// reverse the order
 func reverse(b []byte) []byte {
-	// reverse the order
 	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
 		b[i], b[j] = b[j], b[i]
 	}
 	return b
 }
 
+// Encode returns string encoded by base58
+// arg: byte array
 func Encode(b []byte) string {
 	num := new(big.Int).SetBytes(b)
 	radix := big.NewInt(58)
 
 	ret := make([]byte, 0)
+	// Divide num by 58 and append char corresponded to its remainder until num is less than 58
 	for num.Sign() == 1 {
 		mod := new(big.Int)
 		num.DivMod(num, radix, mod)
@@ -30,11 +33,14 @@ func Encode(b []byte) string {
 	return string(ret)
 }
 
+// Decode returns byte array decoded by base58
+// arg: string
 func Decode(s string) []byte {
 	revstr := string(reverse([]byte(s)))
 	ret := big.NewInt(0)
 	radix := big.NewInt(1)
 	fiftyEight := big.NewInt(58)
+	// Convert base58string to bigint
 	for _, b := range revstr {
 		radix2 := new(big.Int).Set(radix)
 		for j, c := range bitcoinBase58Strings {
